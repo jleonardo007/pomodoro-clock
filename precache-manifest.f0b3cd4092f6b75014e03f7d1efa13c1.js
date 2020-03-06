d80d54522f8c6fa6163068a810297cb66d001bd8
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-03-06 14:23:22 -0400

    Updates

---
+++ precache-manifest.f0b3cd4092f6b75014e03f7d1efa13c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682fb0bcc87cbbebfe4fe8cdef258038",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "b2624597ac081decab84",
+    "url": "/pomodoro-clock/static/css/main.dbed6fda.chunk.css"
+  },
+  {
+    "revision": "05a7763f64ef8b524166",
+    "url": "/pomodoro-clock/static/js/2.1bd86490.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pomodoro-clock/static/js/2.1bd86490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2624597ac081decab84",
+    "url": "/pomodoro-clock/static/js/main.bd507caa.chunk.js"
+  },
+  {
+    "revision": "988ba382437b052e5223",
+    "url": "/pomodoro-clock/static/js/runtime-main.9fd8c30a.js"
+  }
+]);
